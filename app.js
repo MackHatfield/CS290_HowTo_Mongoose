25376b2b3d6f1072d6de31791bafc7221095fcc0
Mack Hatfield <therealbakoonzar@gmail.com>
2018-02-28 21:55:57 +0000

    setup client folder and connect with express.static

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const mongoose = require("mongoose");
 let app = express();
 let {PORT, IP} = process.env;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', express.static((__dirname, 'client')));
+
+
+
 app.listen(PORT, IP, function() {
     console.log(`Server running on PORT: ${PORT} at IP: ${IP}`);
 })
